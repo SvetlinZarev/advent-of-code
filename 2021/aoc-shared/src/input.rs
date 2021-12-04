3a2dfaa60e3f53a157e8450c5d3a320d35140161
Svetlin Zarev <svetlin.zarev@zarev.net>
2021-12-04 18:37:21 +0200

    autodetect the input source

--- 2021/aoc-shared/src/input.rs
+++ 2021/aoc-shared/src/input.rs
@@ -1,5 +1,7 @@
 use std::fmt::Debug;
+use std::fs::File;
 use std::io::Read;
+use std::path::Path;
 use std::str::FromStr;
 use crate::parsing::parse_line_delimited;
 
@@ -11,13 +13,26 @@ pub fn load_input_autodetect() -> String {
     }
 }
 
+pub fn load_line_delimited_input_autodetect<O: FromStr<Err=impl Debug>>() -> Vec<O> {
+    parse_line_delimited(load_input_autodetect())
+}
+
 pub fn stdin_line_delimited<O: FromStr<Err=impl Debug>>() -> Vec<O> {
     let input = load_input_from_stdin();
     parse_line_delimited(input)
 }
 
+pub fn file_line_delimited<O: FromStr<Err=impl Debug>, P: AsRef<Path>>(path: P) -> Vec<O> {
+    let input = load_input_from_file(path);
+    parse_line_delimited(input)
+}
+
 pub fn load_input_from_stdin() -> String {
     load_text_input(std::io::stdin().lock())
+}
+
+pub fn load_input_from_file<P: AsRef<Path>>(path: P) -> String {
+    load_text_input(File::open(path).unwrap())
 }
 
 pub fn load_text_input<R: Read>(mut input: R) -> String {
