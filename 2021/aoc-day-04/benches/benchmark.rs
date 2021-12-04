916c550812cd5d9fcf758572240b60c2aa3be8bc
Svetlin Zarev <svetlin.zarev@zarev.net>
2021-12-04 16:38:25 +0200

    add benchmarks for day-04

---
+++ 2021/aoc-day-04/benches/benchmark.rs
@@ -0,0 +1,44 @@
+use aoc_day_04::{parse_input, part_one, part_two};
+use aoc_shared::input::load_text_input;
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
+use std::fs::File;
+use std::io::BufReader;
+
+criterion_group!(
+    benches,
+    benchmark_parsing,
+    benchmark_part_1,
+    benchmark_part_2
+);
+criterion_main!(benches);
+
+fn benchmark_parsing(c: &mut Criterion) {
+    let file = File::open("inputs/input.txt").unwrap();
+    let input = load_text_input(file);
+
+    c.bench_function("parse-input", |b| {
+        b.iter_batched(
+            || BufReader::new(input.as_bytes()),
+            |input| black_box(parse_input(input)),
+            BatchSize::PerIteration,
+        );
+    });
+}
+
+fn benchmark_part_1(c: &mut Criterion) {
+    let file = File::open("inputs/input.txt").unwrap();
+    let (numbers, boards) = parse_input(BufReader::new(file));
+
+    c.bench_function("part-1", |b| {
+        b.iter(|| black_box(part_one(&numbers, &boards)));
+    });
+}
+
+fn benchmark_part_2(c: &mut Criterion) {
+    let file = File::open("inputs/input.txt").unwrap();
+    let (numbers, boards) = parse_input(BufReader::new(file));
+
+    c.bench_function("part-2", |b| {
+        b.iter(|| black_box(part_two(&numbers, &boards)));
+    });
+}
