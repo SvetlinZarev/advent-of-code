9c30302b09ad2db7f1d6d45c43b9fd9f7a97c55e
Svetlin Zarev <svetlin.zarev@zarev.net>
2021-12-04 12:32:36 +0200

    aoc 2021/04 part 2

--- 2021/aoc-day-04/src/lib.rs
+++ 2021/aoc-day-04/src/lib.rs
@@ -42,25 +42,79 @@ impl Board {
     }
 }
 
+#[derive(Debug, Clone, Default)]
+pub struct Stats {
+    row: [u16; 5],
+    col: [u16; 5],
+    sum: u32,
+    win: bool,
+}
+
+impl Stats {
+    pub fn mark(&mut self, r: u32, c: u32, val: u32) -> bool {
+        self.row[r as usize] += 1;
+        self.col[c as usize] += 1;
+        self.sum += val;
+
+        self.win |= self.row[r as usize] == 5 || self.col[c as usize] == 5;
+        self.win
+    }
+
+    pub fn sum(&self) -> u32 {
+        self.sum
+    }
+
+    pub fn has_won(&self) -> bool {
+        self.win
+    }
+}
+
 pub fn part_one(numbers: &[u32], boards: &[Board]) -> u32 {
     let mut index = vec![Vec::new(); numbers.len()];
     for (idx, board) in boards.iter().enumerate() {
         board.index(&mut index, idx);
     }
 
-    let mut marks = vec![([0; 5], [0; 5], 0u32); boards.len()];
+    let mut marks = vec![Stats::default(); boards.len()];
+
     for number in numbers.iter().copied() {
         for board_idx in index[number as usize].iter().copied() {
             let (r, c) = boards[board_idx].find(number).unwrap();
 
-            marks[board_idx].2 += number;
-            marks[board_idx].1[c as usize] += 1;
-            marks[board_idx].0[r as usize] += 1;
-
-            if marks[board_idx].1[c as usize] == 5 || marks[board_idx].0[r as usize] == 5 {
-                return (boards[board_idx].sum() - marks[board_idx].2) * number;
+            if marks[board_idx].mark(r, c, number) {
+                return (boards[board_idx].sum() - marks[board_idx].sum()) * number;
             }
         }
+    }
+
+    0
+}
+
+pub fn part_two(numbers: &[u32], boards: &[Board]) -> u32 {
+    let mut index = vec![Vec::new(); numbers.len()];
+    for (idx, board) in boards.iter().enumerate() {
+        board.index(&mut index, idx);
+    }
+
+    let mut last_to_win = None;
+    let mut marks = vec![Stats::default(); boards.len()];
+
+    for number in numbers.iter().copied() {
+        for board_idx in index[number as usize].iter().copied() {
+            let stats = &mut marks[board_idx];
+            if stats.has_won() {
+                continue;
+            }
+
+            let (r, c) = boards[board_idx].find(number).unwrap();
+            if stats.mark(r, c, number) {
+                last_to_win = Some((board_idx, number));
+            }
+        }
+    }
+
+    if let Some((board_idx, number)) = last_to_win {
+        return (boards[board_idx].sum() - marks[board_idx].sum()) * number;
     }
 
     0
--- 2021/aoc-day-04/src/main.rs
+++ 2021/aoc-day-04/src/main.rs
@@ -1,4 +1,4 @@
-use aoc_day_04::{parse_input, part_one};
+use aoc_day_04::{parse_input, part_one, part_two};
 use std::io::stdin;
 
 fn main() {
@@ -6,4 +6,5 @@ fn main() {
     let (numbers, boards) = parse_input(std_in.lock());
 
     println!("Part 1: {:?}", part_one(&numbers, &boards));
+    println!("Part 2: {:?}", part_two(&numbers, &boards));
 }
