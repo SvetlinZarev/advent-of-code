1d07f6d536e33d71a0e478566e62588533bfd92e
Svetlin Zarev <svetlin.zarev@zarev.net>
2021-12-04 18:50:26 +0200

    clean-up day 04

--- 2021/aoc-day-04/src/main.rs
+++ 2021/aoc-day-04/src/main.rs
@@ -1,9 +1,9 @@
 use aoc_day_04::{parse_input, part_one, part_two};
-use std::io::stdin;
+use aoc_shared::input::load_input_autodetect;
+use std::io::BufReader;
 
 fn main() {
-    let std_in = stdin();
-    let (numbers, boards) = parse_input(std_in.lock());
+    let (numbers, boards) = parse_input(BufReader::new(load_input_autodetect().as_bytes()));
 
     println!("Part 1: {:?}", part_one(&numbers, &boards));
     println!("Part 2: {:?}", part_two(&numbers, &boards));
