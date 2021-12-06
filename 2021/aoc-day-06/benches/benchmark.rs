0e2192e8a98fc61239ed053c5f3ded074967bfde
Svetlin Zarev <svetlin.zarev@zarev.net>
2021-12-06 17:39:49 +0200

    add manually unrolled array rotation without additional array

--- 2021/aoc-day-06/benches/benchmark.rs
+++ 2021/aoc-day-06/benches/benchmark.rs
@@ -1,4 +1,4 @@
-use aoc_day_06::{part_one_v1, part_one_v2, part_two_v1, part_two_v2};
+use aoc_day_06::{part_one_v1, part_one_v2, part_one_v3, part_two_v1, part_two_v2, part_two_v3};
 use aoc_shared::input::load_text_input_from_file;
 use aoc_shared::parsing::parse_csv;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
@@ -16,6 +16,10 @@ fn benchmark_part_1(c: &mut Criterion) {
     c.bench_function("part-1-v2", |b| {
         b.iter(|| black_box(part_one_v2(black_box(&input))));
     });
+
+    c.bench_function("part-1-v3", |b| {
+        b.iter(|| black_box(part_one_v3(black_box(&input))));
+    });
 }
 
 fn benchmark_part_2(c: &mut Criterion) {
@@ -27,5 +31,9 @@ fn benchmark_part_2(c: &mut Criterion) {
 
     c.bench_function("part-2-v2", |b| {
         b.iter(|| black_box(part_two_v2(black_box(&input))));
+    });
+
+    c.bench_function("part-2-v3", |b| {
+        b.iter(|| black_box(part_two_v3(black_box(&input))));
     });
 }
--- 2021/aoc-day-06/src/lib.rs
+++ 2021/aoc-day-06/src/lib.rs
@@ -14,6 +14,14 @@ pub fn part_two_v2(input: &[u8]) -> u64 {
     solve_v2(input, 256)
 }
 
+pub fn part_one_v3(input: &[u8]) -> u64 {
+    solve_v3(input, 80)
+}
+
+pub fn part_two_v3(input: &[u8]) -> u64 {
+    solve_v3(input, 256)
+}
+
 fn solve_v1(input: &[u8], rounds: usize) -> u64 {
     let mut stage_a = [0u64; 9];
     let mut stage_b = [0u64; 9];
@@ -49,6 +57,27 @@ fn solve_v2(input: &[u8], rounds: usize) -> u64 {
     state.iter().sum()
 }
 
+fn solve_v3(input: &[u8], rounds: usize) -> u64 {
+    let mut fishes = [0u64; 9];
+
+    input.iter().for_each(|&i| fishes[i as usize] += 1);
+
+    for _ in 0..rounds {
+        let gen0 = fishes[0];
+        fishes[0] = fishes[1];
+        fishes[1] = fishes[2];
+        fishes[2] = fishes[3];
+        fishes[3] = fishes[4];
+        fishes[4] = fishes[5];
+        fishes[5] = fishes[6];
+        fishes[6] = fishes[7] + gen0;
+        fishes[7] = fishes[8];
+        fishes[8] = gen0;
+    }
+
+    fishes.iter().sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -68,6 +97,12 @@ mod tests {
         let answer = part_one_v2(&input);
         assert_eq!(351188, answer);
     }
+    #[test]
+    fn test_part_one_v3() {
+        let input = parse_csv(load_text_input_from_file("inputs/input.txt"));
+        let answer = part_one_v3(&input);
+        assert_eq!(351188, answer);
+    }
 
     #[test]
     fn test_part_two_v1() {
@@ -80,6 +115,13 @@ mod tests {
     fn test_part_two_v2() {
         let input = parse_csv(load_text_input_from_file("inputs/input.txt"));
         let answer = part_two_v2(&input);
+        assert_eq!(1595779846729, answer);
+    }
+
+    #[test]
+    fn test_part_two_v3() {
+        let input = parse_csv(load_text_input_from_file("inputs/input.txt"));
+        let answer = part_two_v3(&input);
         assert_eq!(1595779846729, answer);
     }
 }
