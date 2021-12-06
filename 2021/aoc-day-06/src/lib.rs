b8789df4b44d6d4db96d3fe967e9b77abea02be9
Svetlin Zarev <svetlin.zarev@zarev.net>
2021-12-06 12:59:55 +0200

    reduce code duplication

--- 2021/aoc-day-06/src/lib.rs
+++ 2021/aoc-day-06/src/lib.rs
@@ -1,33 +1,26 @@
-pub fn part_one_v1(input: &[u8]) -> u32 {
-    let mut stage_a = [0u32; 9];
-    let mut stage_b = [0u32; 9];
-
-    input.iter().for_each(|&i| stage_a[i as usize] += 1);
-
-    for _ in 0..80 {
-        stage_b[0] = stage_a[1];
-        stage_b[1] = stage_a[2];
-        stage_b[2] = stage_a[3];
-        stage_b[3] = stage_a[4];
-        stage_b[4] = stage_a[5];
-        stage_b[5] = stage_a[6];
-        stage_b[6] = stage_a[7] + stage_a[0];
-        stage_b[7] = stage_a[8];
-        stage_b[8] = stage_a[0];
-
-        std::mem::swap(&mut stage_a, &mut stage_b);
-    }
-
-    stage_a.iter().sum()
+pub fn part_one_v1(input: &[u8]) -> u64 {
+    solve_v1(input, 80)
 }
 
 pub fn part_two_v1(input: &[u8]) -> u64 {
+    solve_v1(input, 256)
+}
+
+pub fn part_one_v2(input: &[u8]) -> u64 {
+    solve_v2(input, 80)
+}
+
+pub fn part_two_v2(input: &[u8]) -> u64 {
+    solve_v2(input, 256)
+}
+
+fn solve_v1(input: &[u8], rounds: usize) -> u64 {
     let mut stage_a = [0u64; 9];
     let mut stage_b = [0u64; 9];
 
     input.iter().for_each(|&i| stage_a[i as usize] += 1);
 
-    for _ in 0..256 {
+    for _ in 0..rounds {
         stage_b[0] = stage_a[1];
         stage_b[1] = stage_a[2];
         stage_b[2] = stage_a[3];
@@ -44,24 +37,12 @@ pub fn part_two_v1(input: &[u8]) -> u64 {
     stage_a.iter().sum()
 }
 
-pub fn part_one_v2(input: &[u8]) -> u32 {
-    let mut state = [0u32; 9];
-
-    input.iter().for_each(|&i| state[i as usize] += 1);
-
-    for idx in 0..80 {
-        state[(idx + 7) % 9] += state[idx % 9];
-    }
-
-    state.iter().sum()
-}
-
-pub fn part_two_v2(input: &[u8]) -> u64 {
+fn solve_v2(input: &[u8], rounds: usize) -> u64 {
     let mut state = [0u64; 9];
 
     input.iter().for_each(|&i| state[i as usize] += 1);
 
-    for idx in 0..256 {
+    for idx in 0..rounds {
         state[(idx + 7) % 9] += state[idx % 9];
     }
 
