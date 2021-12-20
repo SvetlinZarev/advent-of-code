48292293ea87c9b19691ce5a008470f0e1bf0179
Svetlin Zarev <svetlin.zarev@zarev.net>
2021-12-21 01:01:58 +0200

    2021/20 Fix it to work with any input

--- 2021/aoc-day-20/src/solver.rs
+++ 2021/aoc-day-20/src/solver.rs
@@ -1,50 +1,58 @@
 use crate::{Int, Set};
 
+const IDX_9_ONES: usize = (1 << 9) - 1;
+
 pub(crate) fn solve(
     alg: &[u8],
     img: &Set<(Int, Int)>,
     limits: (usize, usize),
     cycles: usize,
 ) -> usize {
-    let mut clr0 = 0;
-    let mut clr1 = alg[0] as usize;
+    // The current state of the "infinite" region. Initially all pixels are 0
+    let mut state = 0;
+
+    // The state of the "infinite" region after the first enhancement
+    let s1 = alg[0] as usize;
+
+    // The state of the "infinite" region after the second enhancement
+    let s2 = if s1 == 0 { alg[0] } else { alg[IDX_9_ONES] } as usize;
 
     // The image will expand with 1 cell per iteration on each side
     let rows = limits.0 + cycles * 2;
     let cols = limits.1 + cycles * 2;
 
-    let mut current = vec![0u8; (rows) * (cols)];
-    let mut next = vec![0u8; current.len()];
+    let mut this_img = vec![0u8; (rows) * (cols)];
+    let mut next_img = vec![0u8; this_img.len()];
 
     // Apply the initial image
     for &(r, c) in img.iter() {
-        set(&mut current, cols, r + cycles, c + cycles, 1);
+        set(&mut this_img, cols, r + cycles, c + cycles, 1);
     }
 
-    for _cl in 0..cycles {
+    for cycle in 0..cycles {
         for r in 0..rows {
             for c in 0..cols {
                 let mut key = 0usize;
-                key |= get(&current, rows, cols, r, c, -1, -1).unwrap_or(clr0) << 8;
-                key |= get(&current, rows, cols, r, c, -1, 0).unwrap_or(clr0) << 7;
-                key |= get(&current, rows, cols, r, c, -1, 1).unwrap_or(clr0) << 6;
-                key |= get(&current, rows, cols, r, c, 0, -1).unwrap_or(clr0) << 5;
-                key |= get(&current, rows, cols, r, c, 0, 0).unwrap_or(clr0) << 4;
-                key |= get(&current, rows, cols, r, c, 0, 1).unwrap_or(clr0) << 3;
-                key |= get(&current, rows, cols, r, c, 1, -1).unwrap_or(clr0) << 2;
-                key |= get(&current, rows, cols, r, c, 1, 0).unwrap_or(clr0) << 1;
-                key |= get(&current, rows, cols, r, c, 1, 1).unwrap_or(clr0) << 0;
+                key |= get(&this_img, rows, cols, r, c, -1, -1).unwrap_or(state) << 8;
+                key |= get(&this_img, rows, cols, r, c, -1, 0).unwrap_or(state) << 7;
+                key |= get(&this_img, rows, cols, r, c, -1, 1).unwrap_or(state) << 6;
+                key |= get(&this_img, rows, cols, r, c, 0, -1).unwrap_or(state) << 5;
+                key |= get(&this_img, rows, cols, r, c, 0, 0).unwrap_or(state) << 4;
+                key |= get(&this_img, rows, cols, r, c, 0, 1).unwrap_or(state) << 3;
+                key |= get(&this_img, rows, cols, r, c, 1, -1).unwrap_or(state) << 2;
+                key |= get(&this_img, rows, cols, r, c, 1, 0).unwrap_or(state) << 1;
+                key |= get(&this_img, rows, cols, r, c, 1, 1).unwrap_or(state) << 0;
 
                 let v = alg[key];
-                set(&mut next, cols, r, c, v);
+                set(&mut next_img, cols, r, c, v);
             }
         }
 
-        std::mem::swap(&mut clr0, &mut clr1);
-        std::mem::swap(&mut current, &mut next);
+        state = if cycle & 1 == 0 { s1 } else { s2 };
+        std::mem::swap(&mut this_img, &mut next_img);
     }
 
-    current.iter().filter(|&&v| v > 0).count()
+    this_img.iter().filter(|&&v| v > 0).count()
 }
 
 fn get(
