f9eca25d63ad572a4d046536e7c47622694ee573
Svetlin Zarev <svetlin.zarev@zarev.net>
2022-12-23 22:14:12 +0200

    2022/23 implement alternative algorithm that does not track duplicate proposals in a separate set

--- 2022/aoc-day-23/benches/benchmark.rs
+++ 2022/aoc-day-23/benches/benchmark.rs
@@ -1,4 +1,4 @@
-use aoc_day_23::{parse_input, part_one, part_two};
+use aoc_day_23::{parse_input, part_one, part_two_v1, part_two_v2};
 use aoc_shared::input::load_text_input_from_file;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
@@ -15,10 +15,16 @@ fn benchmark_part_one(c: &mut Criterion) {
 }
 
 fn benchmark_part_two(c: &mut Criterion) {
+    let mut g = c.benchmark_group("part-2");
+
     let input = load_text_input_from_file("inputs/input.txt");
     let parsed = parse_input(input);
 
-    c.bench_function("part-2", |b| {
-        b.iter(|| part_two(black_box(&parsed)));
+    g.bench_function("v1", |b| {
+        b.iter(|| part_two_v1(black_box(&parsed)));
+    });
+
+    g.bench_function("v2", |b| {
+        b.iter(|| part_two_v2(black_box(&parsed)));
     });
 }
--- 2022/aoc-day-23/src/lib.rs
+++ 2022/aoc-day-23/src/lib.rs
@@ -22,15 +22,15 @@ const PROP: &[(usize, (i32, i32))] = &[(0, (-1, 0)), (1, (1, 0)), (2, (0, -1)), 
 enum Either {
     One((i32, i32)),
     Two(((i32, i32), (i32, i32))),
-    Many(Vec<(i32, i32)>),
 }
 
 impl Either {
     fn append(&mut self, val: (i32, i32)) {
         match self {
             Either::One(old) => *self = Either::Two((*old, val)),
-            Either::Two((a, b)) => *self = Either::Many(vec![*a, *b, val]),
-            Either::Many(vals) => vals.push(val),
+            Either::Two(_) => panic!(
+                "it should not be possible to have more than 2 elves proposing the same spot"
+            ),
         }
     }
 }
@@ -109,12 +109,6 @@ pub fn part_one(input: &[(i32, i32)]) -> i32 {
                     next.insert(a);
                     next.insert(b);
                 }
-
-                Either::Many(prev) => {
-                    prev.into_iter().for_each(|x| {
-                        next.insert(x);
-                    });
-                }
             }
         }
 
@@ -138,7 +132,7 @@ pub fn part_one(input: &[(i32, i32)]) -> i32 {
     area - input.len() as i32
 }
 
-pub fn part_two(input: &[(i32, i32)]) -> usize {
+pub fn part_two_v1(input: &[(i32, i32)]) -> usize {
     let mut board = HashSet::default();
     let mut next = HashSet::default();
     let mut proposed = HashMap::default();
@@ -207,12 +201,6 @@ pub fn part_two(input: &[(i32, i32)]) -> usize {
                     next.insert(a);
                     next.insert(b);
                 }
-
-                Either::Many(prev) => {
-                    prev.into_iter().for_each(|x| {
-                        next.insert(x);
-                    });
-                }
             }
         }
 
@@ -224,11 +212,83 @@ pub fn part_two(input: &[(i32, i32)]) -> usize {
     round
 }
 
+pub fn part_two_v2(input: &[(i32, i32)]) -> usize {
+    let mut board = HashSet::default();
+    let mut next = HashSet::default();
+
+    for elf in input.iter().copied() {
+        board.insert(elf);
+    }
+
+    let mut round = 0;
+
+    loop {
+        let mut moved = false;
+
+        // Propose phase
+        for curr @ (r, c) in board.iter().copied() {
+            // Check which neighbours contain an elf around X
+            //  0 | 1 | 2
+            //  3 | X | 4
+            //  5 | 6 | 7
+            // And store the result in a boolean array `[bool; 8]`
+            let mut neighbours = [false; 8];
+            for (idx, (dr, dc)) in DIR.iter().copied().enumerate() {
+                if board.contains(&(r + dr, c + dc)) {
+                    neighbours[idx] = true;
+                }
+            }
+
+            // Compress the 8 variables to make up the 4 sides
+            let mut sides = [false; 4];
+            sides[0] = neighbours[0] | neighbours[1] | neighbours[2]; //up
+            sides[1] = neighbours[5] | neighbours[6] | neighbours[7]; //down
+            sides[2] = neighbours[0] | neighbours[3] | neighbours[5]; //left
+            sides[3] = neighbours[2] | neighbours[4] | neighbours[7]; //right
+
+            if sides.iter().all(|&x| x) || sides.iter().all(|&x| !x) {
+                // immediately place the elves that stay in place
+                next.insert(curr);
+                continue;
+            }
+
+            moved = true;
+            for (dir, (dr, dc)) in PROP.iter().copied().cycle().skip(round).take(PROP.len()) {
+                let proposed = (r + dr, c + dc);
+                if !sides[dir] {
+                    if !next.insert(proposed) {
+                        // if two elves proposed the same spot, then
+                        // do not move. Push the current position for
+                        // the current elf, and push back the other elf
+                        next.insert(curr);
+                        next.insert((r + 2 * dr, c + 2 * dc));
+
+                        // Remove the duplicate proposal, because no-one will stand there
+                        next.remove(&proposed);
+                    }
+                    break;
+                }
+            }
+        }
+
+        round += 1;
+        if !moved {
+            break;
+        }
+
+        // Move phase; reset for the next round
+        board.clear();
+        swap(&mut next, &mut board);
+    }
+
+    round
+}
+
 #[cfg(test)]
 mod tests {
     use aoc_shared::input::load_text_input_from_file;
 
-    use crate::{parse_input, part_one, part_two};
+    use crate::{parse_input, part_one, part_two_v1, part_two_v2};
 
     #[test]
     fn test_part_one() {
@@ -239,10 +299,18 @@ mod tests {
     }
 
     #[test]
-    fn test_part_two() {
+    fn test_part_two_v1() {
         let input = load_text_input_from_file("inputs/input.txt");
         let parsed = parse_input(input);
-        let answer = part_two(&parsed);
+        let answer = part_two_v1(&parsed);
+        assert_eq!(889, answer);
+    }
+
+    #[test]
+    fn test_part_two_v2() {
+        let input = load_text_input_from_file("inputs/input.txt");
+        let parsed = parse_input(input);
+        let answer = part_two_v2(&parsed);
         assert_eq!(889, answer);
     }
 }
