3f9bed502f97d6c24d4784e4caef88f7922b0f36
Svetlin Zarev <svetlin.zarev@zarev.net>
2023-12-01 01:09:15 +0200

    AoC 2023 template

---
+++ 2023/aoc-day-00/src/main.rs
@@ -0,0 +1,12 @@
+use aoc_day_00::{part_one, part_two};
+use aoc_shared::input::load_text_input_from_autodetect;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = parse_csv(load_text_input_from_autodetect());
+
+    println!("Part 1: {:?}", part_one(&input));
+    println!("Part 2: {:?}", part_two(&input));
+
+    Ok(())
+}
