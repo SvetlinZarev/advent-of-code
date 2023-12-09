4beb92dbc022a62b2a682ba0d8fa3e8563be72e2
Svetlin Zarev <svetlin.zarev@zarev.net>
2023-12-09 18:54:19 +0200

    2023/09 unify part one and two

--- 2023/aoc-day-09/src/lib.rs
+++ 2023/aoc-day-09/src/lib.rs
@@ -10,38 +10,44 @@ pub fn parse_input(input: &str) -> Vec<Vec<i64>> {
 }
 
 pub fn part_one(input: &Vec<Vec<i64>>) -> i64 {
+    solve(input, |n| n[n.len() - 1], extrapolate_last)
+}
+
+pub fn part_two(input: &Vec<Vec<i64>>) -> i64 {
+    solve(input, |n| n[0], extrapolate_first)
+}
+
+fn solve(input: &Vec<Vec<i64>>, select: fn(&[i64]) -> i64, extrapolate: fn(&[i64]) -> i64) -> i64 {
     let mut sum = 0;
 
     let mut nums = vec![];
     let mut next = vec![];
-    let mut last = vec![];
+    let mut vals = vec![];
 
     for line in input.iter() {
+        vals.clear();
         nums.clear();
         nums.extend_from_slice(&line);
 
-        last.clear();
-        last.push(nums[nums.len() - 1]);
-
         loop {
-            let mut more_to_process = false;
             next.clear();
+            vals.push(select(&nums));
 
+            let mut more_to_process = false;
             for w in nums.windows(2) {
                 let diff = w[1] - w[0];
                 more_to_process |= diff != 0;
                 next.push(diff);
             }
 
-            last.push(next[next.len() - 1]);
-            std::mem::swap(&mut nums, &mut next);
-
             if !more_to_process {
                 break;
             }
+
+            std::mem::swap(&mut nums, &mut next);
         }
 
-        sum += extrapolate_last(&last);
+        sum += extrapolate(&vals);
     }
 
     sum
@@ -57,50 +63,10 @@ fn extrapolate_last(nums: &[i64]) -> i64 {
     ext
 }
 
-pub fn part_two(input: &Vec<Vec<i64>>) -> i64 {
-    let mut sum = 0;
-
-    let mut nums = vec![];
-    let mut next = vec![];
-    let mut first = vec![];
-
-    for line in input.iter() {
-        nums.clear();
-        nums.extend_from_slice(&line);
-
-        first.clear();
-        first.push(nums[0]);
-
-        loop {
-            let mut more_to_process = false;
-            next.clear();
-
-            for w in nums.windows(2).rev() {
-                let diff = w[1] - w[0];
-                more_to_process |= diff != 0;
-                next.push(diff);
-            }
-
-            first.push(next[next.len() - 1]);
-
-            next.reverse();
-            std::mem::swap(&mut nums, &mut next);
-
-            if !more_to_process {
-                break;
-            }
-        }
-
-        sum += extrapolate_first(&first);
-    }
-
-    sum
-}
-
 fn extrapolate_first(nums: &[i64]) -> i64 {
     let mut ext = 0;
 
-    for idx in (0..nums.len() - 1).rev() {
+    for idx in (0..nums.len()).rev() {
         ext = nums[idx] - ext
     }
 
