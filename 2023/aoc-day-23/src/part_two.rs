18ac27c428c183afbe93644c3a5515a689ba99a7
Svetlin Zarev <svetlin.zarev@zarev.net>
2023-12-23 23:48:01 +0200

    2023/23 stop the search if we cannot reach the DST node -30% exec time

--- 2023/aoc-day-23/src/part_two.rs
+++ 2023/aoc-day-23/src/part_two.rs
@@ -16,10 +16,12 @@ pub fn part_two(input: &str) -> usize {
     let graph = relabel_graph(&graph, START_RC, (rows - 1, cols - 3));
     assert!(graph.len() < 64);
 
+    let exit_nodes = graph[1].iter().fold(0, |acc, &(id, _)| acc | (1 << id));
+
     // because we have less than 64 nodes in the graph, we can
     // use a bitset to track the visited nodes. Initially,
     // the bitset contains only the starting node
-    longest_path(&graph, 0 | 1 << 0, 0).unwrap()
+    longest_path(&graph, exit_nodes, 0 | 1 << 0, 0).unwrap()
 }
 
 fn compress_graph(
@@ -157,19 +159,38 @@ fn relabel_graph(
     nodes
 }
 
-fn longest_path(graph: &[Vec<(usize, usize)>], seen: usize, node: usize) -> Option<usize> {
+fn longest_path(
+    graph: &[Vec<(usize, usize)>],
+    exit_nodes: usize,
+    seen: usize,
+    node: usize,
+) -> Option<usize> {
     // We fixed the DST node to be always 1 when we relabeled the graph
     const DST_NODE: usize = 1;
 
+    // Base case
     if node == DST_NODE {
         return Some(0);
+    }
+
+    if seen & exit_nodes == exit_nodes {
+        // Small optimisation:
+        // All nodes that are directly connected to the DST node
+        // have been visited, yet, the current node is not the
+        // DST node. Therefore, this path will not yield a result,
+        // because we can never reach the DST node, because we
+        // cannot visit nodes twice. Thus stop searching.
+        return None;
     }
 
     let mut len = None;
 
     for &(next, l) in graph[node].iter() {
         if seen & 1 << next == 0 {
-            if let Some(path) = longest_path(graph, seen | (1 << next), next) {
+            // Note that we are marking the CURRENT node as visited, not the
+            // NEXT node. This is because of the short-circuiting logic
+            // checking for blocked path to the DST node above
+            if let Some(path) = longest_path(graph, exit_nodes, seen | (1 << node), next) {
                 let dist = l + path;
                 len = Some(dist).max(len);
             }
