34cba466b5c37ce6460ad1f47dc7dd65d7d23c12
Svetlin Zarev <svetlin.zarev@zarev.net>
2023-12-25 10:54:30 +0200

    2023/25

--- 2023/aoc-day-25/src/main.rs
+++ 2023/aoc-day-25/src/main.rs
@@ -2,13 +2,12 @@ use std::error::Error;
 
 use aoc_shared::input::load_text_input_from_autodetect;
 
-use aoc_day_00::{part_one, part_two};
+use aoc_day_25::part_one;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = load_text_input_from_autodetect();
 
     println!("Part 1: {:?}", part_one(&input));
-    println!("Part 2: {:?}", part_two(&input));
 
     Ok(())
 }
