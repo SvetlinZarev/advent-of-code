f6b2b8705f478dd036f58c1dfc8ab020e2d38400
Svetlin Zarev <svetlin@zarev.net>
2024-12-11 22:04:30 +0200

    2024/11 do not cache all the numbers, because huge numbers appear less frequently

--- 2024/aoc-day-11/src/lib.rs
+++ 2024/aoc-day-11/src/lib.rs
@@ -1,9 +1,34 @@
 use aoc_shared::hashing::FxHashMap;
+use std::collections::VecDeque;
 
-pub fn part_one(input: &str) -> u64 {
+pub fn part_one_v1(input: &str) -> u64 {
+    let mut buf = input
+        .split_ascii_whitespace()
+        .map(|x| x.parse::<u64>().unwrap())
+        .collect::<VecDeque<u64>>();
+
+    for _ in 0..25 {
+        for _ in 0..buf.len() {
+            let n = buf.pop_front().unwrap();
+
+            if n == 0 {
+                buf.push_back(1);
+            } else if digits(n) % 2 == 0 {
+                buf.push_back(n / 10u64.pow(digits(n) / 2));
+                buf.push_back(n % 10u64.pow(digits(n) / 2));
+            } else {
+                buf.push_back(n * 2024);
+            }
+        }
+    }
+
+    buf.len() as u64
+}
+
+pub fn part_one_v2(input: &str) -> u64 {
     let mut answer = 0;
 
-    let mut cache = FxHashMap::default();
+    let mut cache = Default::default();
     for n in input.split_ascii_whitespace().map(|x| x.parse::<u64>()) {
         answer += dfs(&mut cache, 25, n.unwrap());
     }
@@ -14,7 +39,7 @@ pub fn part_one(input: &str) -> u64 {
 pub fn part_two(input: &str) -> u64 {
     let mut answer = 0;
 
-    let mut cache = FxHashMap::default();
+    let mut cache = Default::default();
     for n in input.split_ascii_whitespace().map(|x| x.parse::<u64>()) {
         answer += dfs(&mut cache, 75, n.unwrap());
     }
@@ -26,12 +51,19 @@ pub fn part_two(input: &str) -> u64 {
 // a single number `number * 100 + remaining`, because `remaining` has a low
 // value that is always less than 100
 fn dfs(cache: &mut FxHashMap<u64, u64>, remaining: u64, number: u64) -> u64 {
+    // The large numbers do not appear that often, so caching
+    // only a portion of the numbers yields a big speed improvement
+    // (73% on my machine)
+    const MAX_NUM_TO_CACHE: u64 = 1024;
+
     if remaining == 0 {
         return 1;
     }
 
-    if let Some(&answer) = cache.get(&(number * 100 + remaining)) {
-        return answer;
+    if number <= MAX_NUM_TO_CACHE {
+        if let Some(&answer) = cache.get(&(number * 100 + remaining)) {
+            return answer;
+        }
     }
 
     let stones = if number == 0 {
@@ -52,7 +84,10 @@ fn dfs(cache: &mut FxHashMap<u64, u64>, remaining: u64, number: u64) -> u64 {
         }
     };
 
-    cache.insert(number * 100 + remaining, stones);
+    if number <= MAX_NUM_TO_CACHE {
+        cache.insert(number * 100 + remaining, stones);
+    }
+
     stones
 }
 
@@ -73,10 +108,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_one() {
+    fn test_part_one_v1() {
         let input = load_text_input_from_file("inputs/input.txt");
 
-        let answer = part_one(&input);
+        let answer = part_one_v1(&input);
+        assert_eq!(222_461, answer);
+    }
+
+    #[test]
+    fn test_part_one_v2() {
+        let input = load_text_input_from_file("inputs/input.txt");
+
+        let answer = part_one_v2(&input);
         assert_eq!(222_461, answer);
     }
 
