f966e543d76ec3e00d452aab008cb6e8af04ba2c
Svetlin Zarev <svetlin@zarev.net>
2024-12-17 09:26:09 +0200

    2024/17

---
+++ 2024/aoc-day-17/src/main.rs
@@ -0,0 +1,15 @@
+use std::error::Error;
+
+use aoc_shared::input::load_text_input_from_autodetect;
+
+use aoc_day_17::{parse_input, part_one, part_two};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = load_text_input_from_autodetect();
+    let parsed = parse_input(&input)?;
+
+    println!("Part 1: {:?}", part_one(&parsed));
+    println!("Part 2: {:?}", part_two(&parsed));
+
+    Ok(())
+}
